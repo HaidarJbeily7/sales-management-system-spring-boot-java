e82879725fe78b3ab440a10a8f94e1955756be80
Haidar Jbeily <haidarjbeily76@gmail.com>
2022-07-13 11:11:59 +0300

    Feat (API, DB): develop the skeleton functionality for products management feature and create the category class to support product representation

---
+++ src/main/java/com/example/demo/Category.java
@@ -0,0 +1,57 @@
+package com.example.demo;
+
+
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @Lob
+    @Nullable
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Long id, String title, @Nullable String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Nullable
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(@Nullable String description) {
+        this.description = description;
+    }
+}
